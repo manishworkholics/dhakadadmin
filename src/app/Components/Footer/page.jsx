6c31a791a721e422e2f64461149cf5c691a53978
manish <manish.workholics@gmail.com>
2026-04-14 18:04:17 +0530

    remove large file

--- src/app/Components/Footer/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-export default page
--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://143.110.244.163:5000" || "",
-});
-
-export default api;
-
